ad29ea255f057ef60dc02c1544b067602ca15843
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-10-19 14:14:40 +0100

    added in enemy

---
+++ FantasyLand/src/main/java/Enemy/Goblin.java
@@ -0,0 +1,9 @@
+package Enemy;
+
+public class Goblin extends Enemy {
+
+
+    public Goblin(String name, double HP, double attackValue) {
+        super(name, HP, attackValue);
+    }
+}
---
+++ FantasyLand/src/main/java/Enemy/Undead.java
@@ -0,0 +1,9 @@
+package Enemy;
+
+public class Undead extends Enemy {
+
+
+    public Undead(String name, double HP, double attackValue) {
+        super(name, HP, attackValue);
+    }
+}
